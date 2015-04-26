a344edcad5dd0f05017ed83f4311a3235924001f
Dan Comand <dcomand@gmail.com>
2015-04-26 18:57:29 -0400

    creating log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+ 
+
 root 'restaurants#index'
   resources :restaurants
   resources :reservations
   resources :users, only: [:new, :create]
+   resources :sessions, only: [:new, :create, :destroy]
 
 end
